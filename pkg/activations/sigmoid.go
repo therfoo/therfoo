1f8135997ed7d5cd2bad9533469217dfa9c4a4d8
David Ndungu <dnjuguna@gmail.com>
2019-01-27 23:18:30 -0800

    adds Sigmoid activation function

---
+++ pkg/activations/sigmoid.go
@@ -0,0 +1,28 @@
+package activations
+
+import (
+	"github.com/therfoo/therfoo/pkg/tensor"
+	"math"
+)
+
+func Sigmoid(z *tensor.Vector) *tensor.Vector {
+	n := z.Len()
+	a := make(tensor.Vector, n, n)
+
+	z.Each(func(index int, value float64) {
+		a[index] = 1. / (1. + math.Exp(-value))
+	})
+
+	return &a
+}
+
+func SigmoidPrime(a *tensor.Vector) *tensor.Vector {
+	n := a.Len()
+	z := make(tensor.Vector, n, n)
+
+	a.Each(func(index int, value float64) {
+		z[index] = value * (1. - value)
+	})
+
+	return &z
+}

c4085f78d0be6071f79828137ebbc62e612cd43a
David Ndungu <dnjuguna@gmail.com>
2019-02-18 00:31:03 -0800

    use activation by reference not copy

--- model/layer.go
+++ model/layer.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Layer interface {
-	Activate(input *tensor.Vector) (output tensor.Vector)
+	Activate(input *tensor.Vector) (output *tensor.Vector)
 	Derive(activation, cost *tensor.Vector)
 	Adjust(delta *[][]float64)
 	Init(neuronsCount, weightsCount int)

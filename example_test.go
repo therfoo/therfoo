424d035c07f6d16f6c7fd449171be50af389a592
David Ndungu <dnjuguna@gmail.com>
2019-02-18 00:37:23 -0800

    adds a Fit example

---
+++ example_test.go
@@ -0,0 +1,32 @@
+package therfoo_example
+
+import (
+	"github.com/therfoo/therfoo/data/basic"
+	"github.com/therfoo/therfoo/layers/dense"
+	"github.com/therfoo/therfoo/model"
+	"github.com/therfoo/therfoo/optimizers/sgd"
+	"github.com/therfoo/therfoo/tensor"
+)
+
+func ExampleFit() {
+	m := model.New(
+		model.WithBinaryAccuracy(),
+		model.WithCrossEntropyLoss(),
+		model.WithEpochs(25),
+		model.WithInputShape(tensor.Shape{2}),
+		model.WithOptimizer(
+			sgd.New(sgd.WithBatchSize(1), sgd.WithLearningRate(0.05)),
+		),
+		model.WithTrainingGenerator(basic.New()),
+		model.WithValidatingGenerator(basic.New()),
+		model.WithVerbosity(true),
+	)
+
+	m.Add(4, dense.New(dense.WithReLU()))
+	m.Add(1, dense.New(dense.WithSigmoid()))
+
+	m.Compile()
+
+	m.Fit()
+
+}

2c46ed6ed40897a804a51b9a6fb23f0a47b62edb
David Ndungu <dnjuguna@gmail.com>
2019-03-16 01:12:56 -0700

    use locks for locking instead of channels

--- optimizers/sgd/sgd.go
+++ optimizers/sgd/sgd.go
@@ -2,6 +2,7 @@ package sgd
 
 import (
 	"github.com/therfoo/therfoo/tensor"
+	"sync"
 )
 
 type Operation func(*SGD)
@@ -10,18 +11,18 @@ type SGD struct {
 	batchSize    int
 	deltas       [][][]float64
 	learningRate float64
-	operations   chan Operation
+	mutex        sync.Mutex
 }
 
 func (o *SGD) Add(layer int, activation, cost *tensor.Vector) {
-	o.operations <- func(d *SGD) {
-		for n := range o.deltas[layer] {
-			for w := range o.deltas[layer][n] {
-				if w == 0 {
-					o.deltas[layer][n][w] += (*cost)[n]
-				} else {
-					o.deltas[layer][n][w] += (*activation)[w-1] * (*cost)[n]
-				}
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+	for n := range o.deltas[layer] {
+		for w := range o.deltas[layer][n] {
+			if w == 0 {
+				o.deltas[layer][n][w] += (*cost)[n]
+			} else {
+				o.deltas[layer][n][w] += (*activation)[w-1] * (*cost)[n]
 			}
 		}
 	}
@@ -39,38 +40,24 @@ func (o *SGD) Init(neurons *[][]int) {
 }
 
 func (o *SGD) Optimizations() *[][][]float64 {
-	done := make(chan struct{})
-
-	o.operations <- func(d *SGD) {
-		for l := range d.deltas {
-			for n := range d.deltas[l] {
-				for p := range d.deltas[l][n] {
-					d.deltas[l][n][p] = d.learningRate * (d.deltas[l][n][p] / float64(d.batchSize))
-				}
+	o.mutex.Lock()
+	defer o.mutex.Unlock()
+	for l := range o.deltas {
+		for n := range o.deltas[l] {
+			for p := range o.deltas[l][n] {
+				o.deltas[l][n][p] = o.learningRate * (o.deltas[l][n][p] / float64(o.batchSize))
 			}
 		}
-		done <- struct{}{}
 	}
-
-	<-done
-
 	return &o.deltas
 }
 
-func (o *SGD) work() {
-	for op := range o.operations {
-		op(o)
-	}
-}
-
 func New(options ...Option) *SGD {
-	o := SGD{operations: make(chan Operation)}
+	o := SGD{}
 
 	for i := range options {
 		options[i](&o)
 	}
-
-	go o.work()
 
 	return &o
 }

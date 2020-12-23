1021f9ce7f79b8cfa7dc7bcae2d38c9f503aa767
David Ndungu <dnjuguna@gmail.com>
2020-12-22 19:46:28 -0800

    adds a Momentum Optimizer

---
+++ pkg/optimizers/momentum.go
@@ -0,0 +1,27 @@
+package optimizers
+
+type Momentum struct {
+	gradients [][]float64
+	momentum  float64
+}
+
+func (m Momentum) Apply(weights [][]float64) Momentum {
+	gradients := make([][]float64, len(weights))
+	for i := range gradients {
+		gradients[i] = make([]float64, len(weights[i]))
+	}
+	return Momentum{
+		gradients: gradients,
+		momentum:  m.momentum,
+	}
+}
+
+func (m Momentum) Optimize(gradients [][]float64) [][]float64 {
+	for i := range gradients {
+		for j := range gradients[i] {
+			m.gradients[i][j] = m.momentum*m.gradients[i][j] + (1-m.momentum)*gradients[i][j]
+		}
+	}
+	// m.gradients[i] = m.momentum*m.gradients[i] + (1-m.momentum)*gradient
+	return m.gradients
+}

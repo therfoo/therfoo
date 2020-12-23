ba8ee696313d626802400b580e2c9585ce282426
David Ndungu <dnjuguna@gmail.com>
2020-12-22 19:46:02 -0800

    adds terminal characters

---
+++ pkg/layers/chars.go
@@ -0,0 +1,18 @@
+package layers
+
+const (
+	cr     = "\033[0K\r"
+	eol    = "\n"
+	nc     = "\033[0m"
+	green  = "\033[0;32m"
+	blue   = "\033[0;34m"
+	brown  = "\033[0;33m"
+	cyan   = "\033[0;36m"
+	purple = "\033[0;35m"
+	red    = "\033[0;31m"
+	white  = "\033[1;37m"
+	yellow = "\033[1;33m"
+	space  = " "
+	indent = "  "
+	tab    = "\t"
+)

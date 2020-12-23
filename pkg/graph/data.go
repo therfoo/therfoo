bb83729127a3db0614ae37189c4416cd74b46497
David Ndungu <dnjuguna@gmail.com>
2020-12-23 01:10:27 -0800

    refactor and add graph.Fitter type

--- pkg/graph/data.go
+++ pkg/graph/data.go
@@ -5,27 +5,38 @@ import (
 )
 
 type Data struct {
-	X [][]float64
-	Y [][]float64
+	ClassWeights        []float64
+	DisableClassWeights bool
+	DisableShuffle      bool
+	X                   [][]float64
+	Y                   [][]float64
 }
 
-func (d Data) ClassWeights() []float64 {
+func (d *Data) Balance() {
 	if len(d.Y) == 0 || len(d.Y[0]) == 0 {
-		return nil
+		return
 	}
 	n := len(d.Y)
 	m := len(d.Y[0])
-	weights := make([]float64, m)
+	d.ClassWeights = make([]float64, m)
 	for i := range d.Y {
-		weights[argmax(d.Y[i])]++
+		d.ClassWeights[argmax(d.Y[i])]++
 	}
-	for i := range weights {
-		weights[i] = float64(n) / (float64(m) * weights[i])
+	for i := range d.ClassWeights {
+		d.ClassWeights[i] = float64(n) / (float64(m) * d.ClassWeights[i])
 	}
-	return weights
 }
 
-func (d Data) Shuffle() {
+func (c *Data) Prepare() {
+	if !c.DisableShuffle {
+		c.Shuffle()
+	}
+	if !c.DisableClassWeights {
+		c.Balance()
+	}
+}
+
+func (d *Data) Shuffle() {
 	r := rand.New(rand.NewSource(DefaultSeed))
 	r.Shuffle(len(d.X), func(i, j int) {
 		d.X[i], d.X[j] = d.X[j], d.X[i]
@@ -33,8 +44,8 @@ func (d Data) Shuffle() {
 	})
 }
 
-func (d Data) Split(split float64) (training, validation Data) {
-	if 0 >= split || split >= 1 {
+func (d *Data) Split(split float64) (training, validation Data) {
+	if 0 > split || split > 1 {
 		panic("validationSplit must be a float between 0 and 1")
 	}
 	n := int64((1 - split) * float64(len(d.X)))

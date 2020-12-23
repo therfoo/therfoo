544174fb3759e6f14e989b881a82ce2edeebd36b
David Ndungu <dnjuguna@gmail.com>
2020-12-22 19:52:36 -0800

    adds graph constants

---
+++ pkg/graph/constants.go
@@ -0,0 +1,8 @@
+package graph
+
+const (
+	DefaultBatchSize    uint64               = 1
+	DefaultLearningRate ConstantLearningRate = 0.01
+	epsilon                                  = 1e-8
+	Seed                                     = 9868
+)
